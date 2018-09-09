a2d3c9aea121fdac8dbfc0ef423ddcf7e57c2d2d
Fabian Untermoser <f.untermoser@gmx.at>
2018-09-09 20:34:41 +0200

    [TASK] use BDD in VendorControllerTest
     - remove bootstrapping from Service class

--- src/test/java/faun/spring/rest/fruitvendorrestapi/fruitvendorrestapi/api/v1/vendors/VendorControllerTest.java
+++ src/test/java/faun/spring/rest/fruitvendorrestapi/fruitvendorrestapi/api/v1/vendors/VendorControllerTest.java
@@ -1,15 +1,22 @@
 package faun.spring.rest.fruitvendorrestapi.fruitvendorrestapi.api.v1.vendors;
 
-import junit.framework.TestCase;
+import faun.spring.rest.fruitvendorrestapi.fruitvendorrestapi.api.v1.vendors.domain.Vendor;
+import faun.spring.rest.fruitvendorrestapi.fruitvendorrestapi.services.VendorService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,10 +24,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = {VendorController.class})
-public class VendorControllerTest extends TestCase {
+public class VendorControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private VendorService vendorService;
+
+    private List<Vendor> vendorResultList;
+
+    @Before
+    public void setUp() {
+        vendorResultList = new ArrayList<>();
+        vendorResultList.add(new Vendor(1L, "first Vendor", VendorController.BASE_API_URL + "/1"));
+        vendorResultList.add(new Vendor(2L, "second Vendor", VendorController.BASE_API_URL + "/2"));
+        vendorResultList.add(new Vendor(3L, "third Vendor", VendorController.BASE_API_URL + "/3"));
+    }
 
     @Test
     public void testReachabillity() throws Exception {
@@ -31,11 +51,12 @@ public class VendorControllerTest extends TestCase {
 
     @Test
     public void testGettingAllVendors() throws Exception {
+        given(vendorService.getAllVendors()).willReturn(vendorResultList);
+
         mockMvc.perform(get(VendorController.BASE_API_URL)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.vendors", hasSize(3)));
     }
-
 
 }
