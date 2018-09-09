49005956c5aef50ff7f40c3a1993da2aa87e9098
Fabian Untermoser <f.untermoser@gmx.at>
2018-09-09 19:28:36 +0200

    [TASK] create project from spring initializr

---
+++ src/main/java/faun/spring/rest/fruitvendorrestapi/fruitvendorrestapi/FruitVendorRestApiApplication.java
@@ -0,0 +1,12 @@
+package faun.spring.rest.fruitvendorrestapi.fruitvendorrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitVendorRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitVendorRestApiApplication.class, args);
+    }
+}
