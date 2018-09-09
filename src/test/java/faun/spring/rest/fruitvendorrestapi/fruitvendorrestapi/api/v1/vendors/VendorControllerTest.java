160186331a009f850738d1da6ba39bd9fbbbba74
Fabian Untermoser <f.untermoser@gmx.at>
2018-09-09 20:01:01 +0200

    [TASK] create VendorControllerTest class

---
+++ src/test/java/faun/spring/rest/fruitvendorrestapi/fruitvendorrestapi/api/v1/vendors/VendorControllerTest.java
@@ -0,0 +1,29 @@
+package faun.spring.rest.fruitvendorrestapi.fruitvendorrestapi.api.v1.vendors;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = {VendorController.class})
+public class VendorControllerTest extends TestCase {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHelloWorld() throws Exception {
+        mockMvc.perform(get(VendorController.BASE_API_URL)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
