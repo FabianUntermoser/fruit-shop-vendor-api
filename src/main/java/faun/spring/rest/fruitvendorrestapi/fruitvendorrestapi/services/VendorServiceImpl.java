58d9931fa81044f7f10d399f5d64565aae0c320f
Fabian Untermoser <f.untermoser@gmx.at>
2018-09-09 20:46:09 +0200

    [REFACTOR] convert from domain to dto in service

--- src/main/java/faun/spring/rest/fruitvendorrestapi/fruitvendorrestapi/services/VendorServiceImpl.java
+++ src/main/java/faun/spring/rest/fruitvendorrestapi/fruitvendorrestapi/services/VendorServiceImpl.java
@@ -1,24 +1,30 @@
 package faun.spring.rest.fruitvendorrestapi.fruitvendorrestapi.services;
 
 import faun.spring.rest.fruitvendorrestapi.fruitvendorrestapi.api.v1.vendors.domain.VendorDTO;
+import faun.spring.rest.fruitvendorrestapi.fruitvendorrestapi.controllers.VendorController;
+import faun.spring.rest.fruitvendorrestapi.fruitvendorrestapi.domain.Vendor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class VendorServiceImpl implements VendorService {
 
-    static final String BASE_API_URL = "/api/v1/vendors";
-
     @Override
     public List<VendorDTO> getAllVendors() {
-        List<VendorDTO> vendors = new ArrayList<>();
+        List<Vendor> vendors = new ArrayList<>();
 
-        vendors.add(new VendorDTO(1L, "first VendorDTO", BASE_API_URL + "/1"));
-        vendors.add(new VendorDTO(2L, "second VendorDTO", BASE_API_URL + "/2"));
-        vendors.add(new VendorDTO(3L, "third VendorDTO", BASE_API_URL + "/3"));
+        vendors.add(new Vendor(1L, "first VendorDTO"));
+        vendors.add(new Vendor(2L, "second VendorDTO"));
+        vendors.add(new Vendor(3L, "third VendorDTO"));
 
-        return vendors;
+        return vendors.stream()
+                .map(vendor -> new VendorDTO(
+                        vendor.getId(),
+                        vendor.getName(),
+                        VendorController.BASE_API_URL + "/" + vendor.getId()
+                )).collect(Collectors.toList());
     }
 }
